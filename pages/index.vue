9fa16417dba2ea7002f5e5a45f655e518a64f64b
kraigochieng <kraigochieng@gmail.com>
2025-07-01 10:41:27 +0300

    added basic content of the page. it might split to other pages. all content will be here first

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<h1>Kraig Ochieng</h1>
+	<h2>Data Scientist | Web Developer</h2>
+	<a href="https://github.com/kraigochieng" target="_blank">Github</a>
+	<a
+		href="https://www.linkedin.com/in/kraig-ochieng-911121215/"
+		target="_blank"
+	>
+		LinkedIn
+	</a>
+	<h2>Projects</h2>
+	<ul>
+		<li>MediLinda. Wed bevelopment with Machine Learning</li>
+	</ul>
+	<h2>Skills</h2>
+	<ul>
+		<li>Web Development</li>
+		<li>Machine Learning</li>
+		<li>Data Analysis</li>
+		<li>Deep Learning</li>
+		<li>Web Scraping</li>
+		<li>AI Agent Development</li>
+		<li>CI/CD</li>
+		<li>Containerisation</li>
+	</ul>
+	<h2>Languages</h2>
+	<ul>
+		<li>Python</li>
+		<li>Javascript</li>
+		<li>SQL</li>
+	</ul>
+	<h2>Hackathons</h2>
+	<ul>
+		<li>KenyaRE</li>
+		<li>Nairobi DSRH</li>
+	</ul>
+	<h2>Experience</h2>
+	<ul>
+		<li>Software Developer | Intellisoft Consulting</li>
+		<li>AI Developer | Chanzo Technologies</li>
+	</ul>
+	<h2>Experience with healthcare</h2>
+	<ul>
+		<li>Experience working with FHIR and FSH</li>
+	</ul>
+</template>
+
+<script setup lang="ts">
+useHead({ title: "Kraig Ochieng | Data scientist and Web Developer" });
+</script>
