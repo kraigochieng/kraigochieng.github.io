dd59e633bd69a0a55440ed869f4bf781687431d7
kraigochieng <kraigochieng@gmail.com>
2025-07-01 10:40:50 +0300

    added the logic for changing between different favicons

--- app.vue
+++ app.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtPage />
+	</div>
 </template>
+
+<script setup lang="ts">
+// Favicon that adjusts to color scheme
+useHead({
+	link: [
+		{
+			rel: "icon",
+			type: "image/svg+xml",
+			href: "/favicon-light-mode.svg",
+			media: "(prefers-color-scheme: light)",
+		},
+		{
+			rel: "icon",
+			type: "image/svg+xml",
+			href: "/favicon-dark-mode.svg",
+			media: "(prefers-color-scheme: dark)",
+		},
+	],
+});
+</script>
