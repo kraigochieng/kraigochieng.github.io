34baa4d14e916b976d55d542d53a027c6d5f9172
kraigochieng <kraigochieng@gmail.com>
2025-07-21 23:58:21 +0300

    added fira code, color mode for dark mode and image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-05-15",
-	devtools: { enabled: false },
-	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
-	googleFonts: {
-		families: {
-			Inter: true,
-		},
-	},
-	devServer: {
-		host: "0.0.0.0",
-	},
+				compatibilityDate: "2025-05-15",
+				devtools: { enabled: false },
+				modules: [
+				 "@nuxtjs/tailwindcss",
+				 "@nuxtjs/google-fonts",
+				 "@nuxtjs/color-mode",
+				 "@nuxt/image",
+				],
+				googleFonts: {
+								families: {
+												Inter: true,
+												"Fira Code": true,
+								},
+				},
+				colorMode: {
+								preference: "system",
+								fallback: "light",
+				},
+				devServer: {
+								host: "0.0.0.0",
+				},
 });
