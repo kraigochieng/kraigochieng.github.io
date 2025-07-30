9ae6ab31bb8b5fa3e21aa3de85df73b91d0756eb
kraigochieng <kraigochieng@gmail.com>
2025-07-30 12:45:14 +0300

    feat(module): added i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				compatibilityDate: "2025-05-15",
-				devtools: { enabled: false },
-				modules: [
-				 "@nuxtjs/tailwindcss",
-				 "@nuxtjs/google-fonts",
-				 "@nuxtjs/color-mode",
-				 "@nuxt/image",
-				],
-				googleFonts: {
-								families: {
-												Inter: true,
-												"Fira Code": true,
-								},
-				},
-				colorMode: {
-								preference: "system",
-								fallback: "light",
-				},
-				devServer: {
-								host: "0.0.0.0",
-				},
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: false },
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/google-fonts",
+		"@nuxtjs/color-mode",
+		"@nuxt/image",
+		"@nuxtjs/i18n",
+	],
+	// Modules
+	googleFonts: {
+		families: {
+			Inter: true,
+			"Fira Code": true,
+		},
+	},
+	colorMode: {
+		preference: "system",
+		fallback: "light",
+	},
+	i18n: {
+		defaultLocale: "en",
+		locales: [{ code: "en", name: "English", file: "en.json" }, { code: "sw", name: "Swahili", file: "sw.json" }],
+	},
+	// Dev Server
+	devServer: {
+		host: "0.0.0.0",
+	},
 });
