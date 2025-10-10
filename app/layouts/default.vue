be7534f0536c1b29061b7eabc3219c4393237b3e
kraigochieng <kraigochieng@gmail.com>
2025-10-11 02:57:38 +0300

    updated navbar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,75 +1,26 @@
 <template>
-	<div class="flex justify-between items-center glass-bg">
-		<UTooltip text="Kraig Ochieng Portfolio">
-			<p
-				@mouseup="router.push('/')"
-				class="italic text-5xl cursor-pointer"
-			>
-				k
-			</p>
-		</UTooltip>
-
+	<div class="flex justify-between items-center sticky-nav glass-bg">
+		<Logo />
 		<Navbar />
 		<div class="flex space-x-2">
-			<ULocaleSelect
-				v-model="locale"
-				:locales="[locales.en]"
-				class="w-32"
-				@update:model-value="val => { if (val) setLocale(val as 'en') }"
-			/>
-
-			<UButton
-				:icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
-				color="neutral"
-				variant="ghost"
-				:aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
-				@click="isDark = !isDark"
-			/>
+			<Locale />
+			<Theme />
 		</div>
 	</div>
 
 	<slot />
 </template>
 
-<script setup lang="ts">
-import * as locales from "@nuxt/ui/locale";
-import type { SelectItem } from "@nuxt/ui";
-const router = useRouter();
-const { locale, setLocale } = useI18n();
-
-const items = ref([
-	{
-		label: "System",
-		value: "system",
-		icon: "i-lucide-monitor",
-	},
-	{
-		label: "Light",
-		value: "light",
-		icon: "i-lucide-sun",
-	},
-	{
-		label: "Dark",
-		value: "dark",
-		icon: "i-lucide-moon",
-	},
-] satisfies SelectItem[]);
-
-const colorMode = useColorMode();
-const isDark = computed({
-	get() {
-		return colorMode.value === "dark";
-	},
-	set(_isDark) {
-		colorMode.preference = _isDark ? "dark" : "light";
-	},
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 @reference "assets/css/main.css";
 
 .glass-bg {
 	@apply backdrop-filter backdrop-blur-xs;
+}
+
+.sticky-nav {
+	@apply sticky top-0 z-50;
 }
 </style>
