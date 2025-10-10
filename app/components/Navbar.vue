c2c7b010089f8bedc692058c7b33c753bd075600
kraigochieng <kraigochieng@gmail.com>
2025-10-11 02:43:31 +0300

    added navbar component

---
+++ app/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+	 <UNavigationMenu :items="items" class="w-full justify-center" />
+</template>
+
+<script lang="ts" setup>
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items = ref<NavigationMenuItem[]>([
+	{
+		label: "Home",
+		to: "/",
+	},
+	{
+		label: "Projects",
+		to: "/projects",
+	},
+]);
+</script>
+
+<style></style>
