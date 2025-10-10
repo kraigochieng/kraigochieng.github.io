a46def14cfadc63f6b0f848e898c84ee7ed5da86
kraigochieng <kraigochieng@gmail.com>
2025-10-11 02:43:52 +0300

    updated light setting component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,18 +1,31 @@
 <template>
-	<div class="flex justify-around glass-bg">
-		<NuxtLink to="/">Home</NuxtLink>
-		<ULocaleSelect
-			v-model="locale"
-			:locales="Object.values(locales)"
-			class="w-48"
-		/>
-		<USelect
-			v-model="value"
-			:items="items"
-			value-key="value"
-			:icon="icon"
-			class="w-40"
-		/>
+	<div class="flex justify-between items-center glass-bg">
+		<UTooltip text="Kraig Ochieng Portfolio">
+			<p
+				@mouseup="router.push('/')"
+				class="italic text-5xl cursor-pointer"
+			>
+				k
+			</p>
+		</UTooltip>
+
+		<Navbar />
+		<div class="flex space-x-2">
+			<ULocaleSelect
+				v-model="locale"
+				:locales="[locales.en]"
+				class="w-32"
+				@update:model-value="val => { if (val) setLocale(val as 'en') }"
+			/>
+
+			<UButton
+				:icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+				color="neutral"
+				variant="ghost"
+				:aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
+				@click="isDark = !isDark"
+			/>
+		</div>
 	</div>
 
 	<slot />
@@ -21,10 +34,8 @@
 <script setup lang="ts">
 import * as locales from "@nuxt/ui/locale";
 import type { SelectItem } from "@nuxt/ui";
-
-const locale = ref("en");
-
-const colorMode = useColorMode();
+const router = useRouter();
+const { locale, setLocale } = useI18n();
 
 const items = ref([
 	{
@@ -44,14 +55,15 @@ const items = ref([
 	},
 ] satisfies SelectItem[]);
 
-const value = computed({
-	get: () => colorMode.preference,
-	set: (v) => (colorMode.preference = v),
+const colorMode = useColorMode();
+const isDark = computed({
+	get() {
+		return colorMode.value === "dark";
+	},
+	set(_isDark) {
+		colorMode.preference = _isDark ? "dark" : "light";
+	},
 });
-
-const icon = computed(
-	() => items.value.find((item) => item.value === value.value)?.icon
-);
 </script>
 
 <style scoped>
