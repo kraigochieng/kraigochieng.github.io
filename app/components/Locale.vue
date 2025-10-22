433eecf0f26b91ce13c954e47b321ffa403cc790
kraigochieng <kraigochieng@gmail.com>
2025-10-22 21:27:34 +0300

    removed colr mode items

--- app/components/Locale.vue
+++ app/components/Locale.vue
@@ -9,27 +9,8 @@
 
 <script lang="ts" setup>
 import * as locales from "@nuxt/ui/locale";
-import type { SelectItem } from "@nuxt/ui";
 
 const { locale, setLocale } = useI18n();
-
-const items = ref([
-	{
-		label: "System",
-		value: "system",
-		icon: "i-lucide-monitor",
-	},
-	{
-		label: "Light",
-		value: "light",
-		icon: "i-lucide-sun",
-	},
-	{
-		label: "Dark",
-		value: "dark",
-		icon: "i-lucide-moon",
-	},
-] satisfies SelectItem[]);
 </script>
 
 <style></style>
