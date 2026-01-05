c44f40723d7bc27d3e3424354026d8565191ae7b
kraigochieng <kraigochieng@gmail.com>
2026-01-05 13:44:04 +0300

    feat: added project type

---
+++ app/types/index.d.ts
@@ -0,0 +1,10 @@
+export interface Project {
+	name: string;
+	slug: string;
+	domain: string[];
+	description: string;
+	skills: string[];
+	tools: string[];
+	link: string;
+	github: string | null;
+}
