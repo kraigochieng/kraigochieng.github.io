61aef3c9bfe87457d99227bb28b422efe94ba683
kraigochieng <kraigochieng@gmail.com>
2026-08-22 22:45:29 +0300

    fix: replaced letter k with name

--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -1,13 +1,24 @@
 <template>
-	<UTooltip text="Kraig Ochieng Portfolio">
-		<p @mouseup="router.push('/')" class="italic text-5xl cursor-pointer">
+	<UTooltip :text="t('portfolio_tooltip')">
+		<!-- <p
+			@mouseup="router.push(localePath('/'))"
+			class="italic text-5xl cursor-pointer"
+		>
 			k
+		</p> -->
+		<p
+			@mouseup="router.push(localePath('/'))"
+			class="italic text-2xl font-semibold cursor-pointer whitespace-nowrap"
+		>
+			Kraig Ochieng
 		</p>
 	</UTooltip>
 </template>
 
 <script lang="ts" setup>
 const router = useRouter();
+const localePath = useLocalePath();
+const { t } = useI18n();
 </script>
 
 <style></style>
